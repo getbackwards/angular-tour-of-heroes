d8af89dd2917f93c0c307a45658cc09fe0be175a
Your Name <getbackwards@gmail.com>
2020-04-22 08:43:39 +1200

    adding of

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
